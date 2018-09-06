6f609cc0cc5784dc0b0565f8aadc262bced68a73
Zylon-D-Lite <zylon.d.lite@gmail.com>
2018-09-07 03:26:39 +0630

    Initial commit

---
+++ js_me.js
@@ -0,0 +1,23 @@
+function gotoProgrammer() {
+	document.getElementById("programmer").scrollIntoView({
+		behavior: 'smooth'
+	});
+}
+
+function gotoWebdev() {
+	document.getElementById("webdev").scrollIntoView({
+		behavior: 'smooth'
+	});
+}
+
+function gotoTeacher() {
+	document.getElementById("teacher").scrollIntoView({
+		behavior: 'smooth'
+	});
+}
+
+function gotoHead() {
+	document.getElementById("head_panel").scrollIntoView({
+		behavior: 'smooth'
+	});
+}
